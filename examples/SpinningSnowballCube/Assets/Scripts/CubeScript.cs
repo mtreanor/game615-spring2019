6da5b884ee3baaa5f20785e18ceda6b43121706f
Mike Treanor <micitari@gmail.com>
2019-02-07 14:58:59 -0500

    Added exercise 4 and examples

---
+++ examples/SpinningSnowballCube/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+	public GameObject snowballPrefab;
+
+	float speed = 5f;
+	float rotateSpeed = 200;
+	//public GameObject target;
+	bool shouldRotate = false;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//This line is just to demonstrate how we can print out the values of variables to
+		//the console. This is useful for debugging because it allows us to see the values
+		//of variables while the game is running. This example isn't particularly useful, 
+		//but it does show how to use Debug.Log().
+		Debug.Log(transform.forward);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (shouldRotate) {
+			transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+		}
+
+		//As long as the player is holding x, create snowball prefabs at the current position
+		//of the player, and facing in the same direction as the player.
+		if (Input.GetKey(KeyCode.X)) {
+			GameObject snowball = Instantiate(snowballPrefab, transform.position, transform.rotation);
+			//This will tell Unity to destroy the snowball in 20 seconds.
+			Destroy(snowball, 20f);
+		}
+	}
+
+	//This function is called by the 'spin' button when it is clicked. Look at the button and
+	//review how this is set.
+	public void ChangeShouldRotate() {
+		//Set the shouldRotate boolean variable to be the opposite of what it was
+		shouldRotate = !shouldRotate;
+	}
+}
---
+++ examples/SpinningSnowballCube/Assets/Scripts/SnowballScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballScript : MonoBehaviour
+{
+	public float speed = 5f;
+	GameObject playerObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Get a reference to the player game object.
+		//We weren't able to manual assign the value to playerObj in the Unity editor
+		//because the snowball didn't exist before the game was running.
+		//NOTE: There are other ways to accomplish this same thing, and this example
+		//is meant to just show how GameObject.Find() works.
+		playerObj = GameObject.Find("Player");
+
+		//Create a random number between 0 and 1.5
+		float randomScale = Random.value * 1.5f;
+		//Overwrite the transform's scale to be randomScale for x, y, and z
+		transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+
+		//Give the snowball a random color, but modifying the game object's
+		//material's color value.
+		Renderer renderer = gameObject.GetComponent<Renderer>();
+		//This creates a variable that holds a color object with random rgb values
+		Color randomColor = new Color(Random.value, Random.value, Random.value);
+		//Assign the value (this is the line that actually changes the color)
+		renderer.material.color = randomColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//Move forward
+		//We multiple by Time.deltaTime to ensure continuous movement
+		//We can control the speed by modifying the speed variable
+		transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+
+		//When the player presses Z "Attack" (i.e. start moving toward the player)
+		//LookAt rotates the transform so that the forward vector is facing the player
+		if (Input.GetKeyDown(KeyCode.Z)) {
+			//The code below demonstrates how to compute a vector from the snowball to 
+			//the player.
+			//Vector3 vectorToTarget = playerObj.transform.position - transform.position;
+			//vectorToTarget = vectorToTarget.normalized;
+
+			//There are situations where doing something like the above is useful, but in this
+			//case, it is easier to just have the snowball 'LookAt' the player
+			transform.LookAt(playerObj.transform);
+		}
+
+		//When the player presses C, "Go Nuts"
+		//In other words, rotate a random amount in the x, y, and z directions. 
+		//Once this happens, the forward movement of the snowball will send it off in 
+		//a random direction.
+		if (Input.GetKeyDown(KeyCode.C)) {
+			transform.Rotate(Random.value * 360, Random.value * 360, Random.value * 360);
+		}
+	}
+}
