e4a2cf8b2812516dd24d75fa6524667d289873bc
Mike Treanor <micitari@gmail.com>
2019-02-13 11:53:08 -0500

    Added round 1 of mike's ex04

---
+++ examples/mikesExercise04/Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	float moveSpeed = 5f;
+	float rotateSpeed = 40f;
+
+	public Rigidbody rigidbody;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.DrawRay(transform.position, transform.forward * 10);
+
+		float hAxis = Input.GetAxis("Horizontal");
+		float vAxis = Input.GetAxis("Vertical");
+
+		transform.Rotate(0, hAxis * rotateSpeed * Time.deltaTime, 0);
+		transform.Translate(transform.forward * vAxis * moveSpeed * Time.deltaTime, Space.World);
+	}
+}
---
+++ examples/mikesExercise04/Assets/RainMaker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainMaker : MonoBehaviour
+{
+	float dropRate;
+	float dropTimer;
+
+	public GameObject dropPrefab;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		dropRate = 0.4f;
+		dropTimer = dropRate;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		dropTimer -= Time.deltaTime;
+		if (dropTimer < 0) {
+			makeDrop();
+			makeDrop();
+			dropTimer = dropRate;
+		}
+	}
+	void makeDrop()
+	{
+		float x = transform.position.x + Random.Range(-transform.localScale.x / 2 * 10, transform.localScale.x / 2 * 10);
+		float z = transform.position.z + Random.Range(-transform.localScale.z / 2 * 10, transform.localScale.z / 2 * 10);
+		Vector3 pos = new Vector3(x, transform.position.y + 5, z);
+
+		GameObject drop = Instantiate(dropPrefab, pos, Quaternion.identity);
+		Material dropMat = drop.GetComponent<Renderer>().material;
+		dropMat.color = new Color(Random.value, Random.value, Random.value);
+
+		drop.transform.Rotate(0, Random.Range(0, 360), 0);
+		Rigidbody dropRB = drop.GetComponent<Rigidbody>();
+		dropRB.AddForce(transform.forward * Random.Range(0, 300));
+	}
+
+}
